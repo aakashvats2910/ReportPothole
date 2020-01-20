067b232e08234d8c15c649e2406f219c29a211a5
Aakash Sharma <aakashvats2910@gmail.com>
2020-01-20 21:11:01 +0530

    Complete first model

--- app/src/main/java/com/myidea/sih/reportpothole/CivilInsideActivity.java
+++ app/src/main/java/com/myidea/sih/reportpothole/CivilInsideActivity.java
@@ -4,7 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -18,6 +20,7 @@ import com.myidea.sih.reportpothole.complaint.UserComplaint;
 import com.myidea.sih.reportpothole.data_helper.DataFramework;
 import com.myidea.sih.reportpothole.database.Fire;
 import com.myidea.sih.reportpothole.util.AgencyName;
+import com.myidea.sih.reportpothole.util.UserComplaintToShow;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,13 +39,17 @@ public class CivilInsideActivity extends AppCompatActivity {
 
         just_civil_list_view = findViewById(R.id.just_civil_list_view);
 
-        civilComplaintAdapter = new CivilComplaintAdapter(CivilInsideActivity.this, R.layout.civil_list_layout, DataFramework.userComplaintList);
+        // Importing the value from data framework to the global variable in this class.
+        userComplaintList = DataFramework.userComplaintList;
+
+        civilComplaintAdapter = new CivilComplaintAdapter(CivilInsideActivity.this, R.layout.civil_list_layout, userComplaintList);
         just_civil_list_view.setAdapter(civilComplaintAdapter);
 
         just_civil_list_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                UserComplaintToShow.toShow = userComplaintList.get(position);
+                startActivity(new Intent(CivilInsideActivity.this, CivilSeeFullComplaint.class));
             }
         });
 
--- app/src/main/java/com/myidea/sih/reportpothole/CivilSeeFullComplaint.java
+++ app/src/main/java/com/myidea/sih/reportpothole/CivilSeeFullComplaint.java
@@ -2,12 +2,8 @@ package com.myidea.sih.reportpothole;
 
 import androidx.fragment.app.FragmentActivity;
 
-import android.content.Intent;
-import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,42 +16,35 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.StorageReference;
-import com.myidea.sih.reportpothole.complaint.UserComplaint;
 import com.myidea.sih.reportpothole.database.Fire;
 import com.myidea.sih.reportpothole.util.UserComplaintToShow;
 
-import org.w3c.dom.Text;
+public class CivilSeeFullComplaint extends FragmentActivity implements OnMapReadyCallback {
 
-public class GovtSeeFullComplaint extends FragmentActivity implements OnMapReadyCallback {
+    private ImageView civil_fullcomplaint_image_view;
+    private TextView civil_fullcomplaint_name_text_view;
+    private TextView civil_fullcomplaint_number_text_view;
+    private TextView civil_fullcomplaint_comment_text_view;
 
     private GoogleMap mMap;
-
-    private ImageView fullcomplaint_image_view;
-    private TextView fullcomplaint_name_text_view;
-    private TextView fullcomplaint_number_text_view;
-    private TextView fullcomplaint_comment_text_view;
-    private Button fullcomplaint_choose_agency_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_govt_see_full_complaint);
+        setContentView(R.layout.activity_civil_see_full_complaint);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        fullcomplaint_image_view = findViewById(R.id.fullcomplaint_image_view);
-        fullcomplaint_name_text_view = findViewById(R.id.fullcomplaint_name_text_view);
-        fullcomplaint_number_text_view = findViewById(R.id.fullcomplaint_number_text_view);
-        fullcomplaint_comment_text_view = findViewById(R.id.fullcomplaint_comment_text_view);
-        fullcomplaint_choose_agency_button = findViewById(R.id.fullcomplaint_choose_agency_button);
+        civil_fullcomplaint_name_text_view = findViewById(R.id.civil_fullcomplaint_name_text_view);
+        civil_fullcomplaint_number_text_view = findViewById(R.id.civil_fullcomplaint_number_text_view);
+        civil_fullcomplaint_comment_text_view = findViewById(R.id.civil_fullcomplaint_comment_text_view);
+        civil_fullcomplaint_image_view = findViewById(R.id.civil_fullcomplaint_image_view);
 
-        //todo image left to add in the image view.
-
-        fullcomplaint_name_text_view.setText(UserComplaintToShow.toShow.complainerName);
-        fullcomplaint_number_text_view.setText(UserComplaintToShow.toShow.mobileNumber);
-        fullcomplaint_comment_text_view.setText(UserComplaintToShow.toShow.comment);
+        civil_fullcomplaint_name_text_view.setText(UserComplaintToShow.toShow.complainerName);
+        civil_fullcomplaint_number_text_view.setText(UserComplaintToShow.toShow.mobileNumber);
+        civil_fullcomplaint_comment_text_view.setText(UserComplaintToShow.toShow.comment);
 
         StorageReference storageReference = Fire.mainStorageReference.child(UserComplaintToShow.toShow.imageUniqueID);
         storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
@@ -63,14 +52,7 @@ public class GovtSeeFullComplaint extends FragmentActivity implements OnMapReady
             public void onSuccess(Uri uri) {
                 Glide.with(getApplicationContext())
                         .load(uri)
-                        .into(fullcomplaint_image_view);
-            }
-        });
-
-        fullcomplaint_choose_agency_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(GovtSeeFullComplaint.this, GovtSelectAgencyActivity.class));
+                        .into(civil_fullcomplaint_image_view);
             }
         });
 
@@ -91,9 +73,8 @@ public class GovtSeeFullComplaint extends FragmentActivity implements OnMapReady
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-
-        LatLng markedLocatoin = new LatLng(UserComplaintToShow.toShow.lat, UserComplaintToShow.toShow.lng);
-        mMap.addMarker(new MarkerOptions().position(markedLocatoin).title("Pothole"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(markedLocatoin, 18f));
+        LatLng potholeLocation = new LatLng(UserComplaintToShow.toShow.lat, UserComplaintToShow.toShow.lng);
+        mMap.addMarker(new MarkerOptions().position(potholeLocation).title("Pothole"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(potholeLocation, 18f));
     }
 }
